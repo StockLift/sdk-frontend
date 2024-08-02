6fd929959425934fd32acd7acf11f5494519fe58
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2024-08-02 17:24:30 +0700

    test

--- test-003.js
+++ test-003.js
@@ -1,7 +1,10 @@
-export default class Three {
-  constructor() {
-    console.log(`Imported class 003`)
-  }
+function Three(apiKey) {
+  console.log(`** Imported func 003`)
+  console.log(`apiKey: ${apiKey}`)
 }
 
 console.log(`Imported file 003`)
+const options = {
+  apiKey: 'test-003',
+  uuid: 'test-003'
+}
